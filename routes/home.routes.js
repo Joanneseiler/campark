598144d4067dbbbe6e62d57507b2bf702753ee1b
Your Name <you@example.com>
2021-07-07 12:56:02 +0200

    deploy

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 router.get("/", (req, res, next) => {
 
   let profilePic = "images/default-avatar.png"
-  console.log("home " + req.user)
   if (req.app.locals.isLoggedIn && !req.user === null && !req.user.profilePic === null) {
      profilePic = req.user.profilePic
   }
