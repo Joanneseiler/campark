f4045dd271a39e9423fcefc4623b37aa5eb054cc
Joanne Seiler <joanne@posteo.de>
2021-07-09 09:36:23 +0200

    try to fix bug

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -5,6 +5,7 @@ router.get("/", (req, res, next) => {
   // if (req.app.locals.isLoggedIn) {
   //    profilePic = req.session.loggedInUser.profilePic
   // }  
+  let profilePic
 
   if (req.app.locals.isLoggedIn) {
     if(req.session.loggedInUser.profilePic == 'images/default-avatar.png') {
@@ -12,7 +13,7 @@ router.get("/", (req, res, next) => {
     } else {
         profilePic = req.session.loggedInUser.profilePic
     }
-} 
+  } 
   res.render("home/home", {title: "Home", profilePic: profilePic});
 });
 
